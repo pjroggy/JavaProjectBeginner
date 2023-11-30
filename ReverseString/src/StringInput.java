9b6fb3c0e7a347232270b78d0fcf9729454eb9b8
pjroggy <pierrejeanroggy@gmail.com>
2023-11-30 15:18:25 +0100

    ajout de la fonction input et usage dans les 5 différentes classes reverseString

---
+++ ReverseString/src/StringInput.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class StringInput {
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static String getUserInput(){
+        System.out.print("Entrez une chaine de caractère ; ");
+        return scanner.nextLine();
+    }
+    public static void closeScanner(){
+        scanner.close();
+    }
+}
