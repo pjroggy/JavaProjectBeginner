b82cd2a9eec6f2d8cebee4ead0744040b929f221
DavidG <david.giangiacomo@evooq.ch>
2024-01-17 12:28:43 +0100

    Update temperature converter to use class and enum

--- Fahrenheit-Celsius Converter/src/FahrenheitCelsiusConverter.java
+++ Fahrenheit-Celsius Converter/src/FahrenheitCelsiusConverter.java
@@ -1,24 +1,25 @@
 import java.util.Scanner;
 
 public class FahrenheitCelsiusConverter {
-    private static double celsiusToFahrenheit(double celsius) {
-        return (celsius * 9 / 5) + 32;
+    private final Scanner scanner;
+    private final TemperatureConverter converter;
+
+    public FahrenheitCelsiusConverter(Scanner scanner, TemperatureConverter converter) {
+        this.scanner = scanner;
+        this.converter = converter;
     }
 
-    private static double fahrenheitToCelsius(double fahrenheit) {
-        return (fahrenheit - 32) * 5 / 9;
+    private void conversion() {
+        System.out.println("Entrez la température en %s".formatted(converter.source().displayName));
+        double temperature = scanner.nextDouble();
+        System.out.println("Egal à : %s %s".formatted(converter.convert(temperature), converter.target().displayName));
     }
 
     public static void main(String[] args) {
-
         Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Entrez la température en °C :");
-        double temperatureCelsiue = scanner.nextDouble();
-        System.out.println("Egal à : " + celsiusToFahrenheit(temperatureCelsiue) + " °F");
-
-        System.out.println("Entrez la température en °F :");
-        double temperatureFahrenheit = scanner.nextDouble();
-        System.out.println("Egal à : " + fahrenheitToCelsius(temperatureFahrenheit) + " °C");
+        new FahrenheitCelsiusConverter(scanner, new CelsiusToFahrenheitConverter())
+            .conversion();
+        new FahrenheitCelsiusConverter(scanner, new FahrenheitToCelsiusConverter())
+            .conversion();
     }
 }
